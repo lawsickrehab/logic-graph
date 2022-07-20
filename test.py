c3ee6c0e374a47e14890b6e8f75f0fca6abbdbdd
ycwang <ycwang.cs@gmail.com>
2022-07-20 23:22:51 +0800

    txt files added

--- test.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfReader
-from isort import file
-class pdfHandler:
-    def pdf2txt(filename):
-        reader = PdfReader("src/" + filename)
-        number_of_pages = len(reader.pages)
-        page = reader.pages[0]
-        text = page.extract_text()
-        return text
