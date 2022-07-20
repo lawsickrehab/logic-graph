4703015c72d7f85addf7b124b99ab89dc612ce2b
ycwang <ycwang.cs@gmail.com>
2022-07-20 21:45:47 +0800

    pdf2txt

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from PyPDF2 import PdfReader
+from isort import file
 class pdfHandler:
     def pdf2txt(filename):
-    reader = PdfReader("example.pdf")
-    number_of_pages = len(reader.pages)
-    page = reader.pages[0]
-    text = page.extract_text()
+        reader = PdfReader("src/" + filename)
+        number_of_pages = len(reader.pages)
+        page = reader.pages[0]
+        text = page.extract_text()
+        return text
