5e8b1ab1d44976bc11d9e62746aba014671d02e5
ZoLArk173 <clark.20011120@gmail.com>
2022-07-20 23:10:11 +0800

    Add more regx

--- filter.py
+++ filter.py
@@ -1,12 +1,19 @@
 import re
 
 class filter:
-    regxs = [
-        '[，,。]按*。',
-        '[，,。]而*。',
-        '固然*；然*自*。',
-        '具體言之*可以*加以綜合判斷*。'
-    ]
 
-    def find(self, text: str, regx):
-        return re.search(regx, text)
+    regxs = [
+        '[，。；]按.+?。',
+        '[，。；]而.+?。',
+        '固然.+?，然[^。]+?。', # 不會選最近的句號
+        '具體言之.+?[，。]可以.+?加以綜合判斷',
+        '尚應權衡.+?為一整體性判斷',
+        '倘.+?則.+?但.+?。',
+        '按.+?[，。]除.+?。'
+    ]
+    
+    def find(self, regx, text: str):
+        return re.findall(regx, text)
+
+
+# hell
