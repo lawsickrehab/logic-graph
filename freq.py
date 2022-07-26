a85cbe298d12b9e9dc7781ca10d6af6f91b9b39b
ZoLArk173 <clark.20011120@gmail.com>
2022-07-26 16:38:59 +0800

    Calc the frequency of nouns for every related laws

--- freq.py
+++ freq.py
@@ -13,6 +13,12 @@ countProgress = 0
 for textFile in textFiles:
     countProgress += 1
     print(f'{folderPath}/{textFile}', f'{countProgress}/{len(textFiles)}')
+
+    if textFile in cache:
+        continue
+    if textFile[-5:] != '.json':
+        continue
+
     with open(f'{folderPath}/{textFile}') as file:
         judgements = json.loads(file.read())
         api.parse(judgements["judgement"])
@@ -21,16 +27,47 @@ for textFile in textFiles:
 # %%
 print(cache)
 # %%
-nounCount = {}
+lawNounCount = {}
 for fileName, sentences in cache.items():
+    print(fileName, sentences)
+    if sentences == None:
+        continue
+    with open(f'{folderPath}/{textFile}') as file:
+        judgements = json.loads(file.read())
+    relatedLaws = judgements['relatedIssues']
+    relatedKeys = []
+
+    for law in relatedLaws:
+        key = f"{law['lawName']}-{law['issueRef']}"
+        relatedKeys.append(key)
+        if key not in lawNounCount:
+            lawNounCount[key] = {}
+    
     for sentence in sentences:
         for noun in sentence:
-            if noun[-1] not in nounCount:
-                nounCount[noun[-1]] = 1
-            else:
-                nounCount[noun[-1]] += 1
+            for key in relatedKeys:
+                if noun[-1] not in lawNounCount[key]:
+                    lawNounCount[key][noun[-1]] = 1
+                else:
+                    lawNounCount[key][noun[-1]] += 1
 
-print(nounCount)
+print(lawNounCount)
 # %%
 {k: v for k, v in sorted(nounCount.items(), key=lambda item: item[1])}
+
+
+
+
+
+
+# %%
+def saveCache(cache) -> None:
+    with open('cache.json', 'w') as file:
+        json.dump(cache, file)
+saveCache(cache)
+# %%
+def loadCache() -> dict:
+    with open('cache.json', 'r') as file:
+        return json.loads(file.read())
+cache = loadCache()
 # %%
